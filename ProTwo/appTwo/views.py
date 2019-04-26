1f8a5d79acffb7b5eb4fb880b5b7d83a699b2391
SofijnConsultingGroup <33628307+SofijnConsultingGroup@users.noreply.github.com>
2019-04-26 21:55:51 +0200

    syntax error weg

--- ProTwo/appTwo/views.py
+++ ProTwo/appTwo/views.py
@@ -29,9 +29,9 @@ def add_users(request):
         form = forms.NewUserForm(request.POST)
         if form.is_valid():
             print("validation succesfull!")
-            print(f"voornaam: {form.cleaned_data['first_name']}")
-            print(f"achternaam: {form.cleaned_data['last_name']}")
-            print(f"email: {form.cleaned_data['e_mail']}")
+            # print(f"voornaam: {form.cleaned_data['first_name']}")
+            # print(f"achternaam: {form.cleaned_data['last_name']}")
+            # print(f"email: {form.cleaned_data['e_mail']}")
 
             form.save(commit=True)
             return users(request)
