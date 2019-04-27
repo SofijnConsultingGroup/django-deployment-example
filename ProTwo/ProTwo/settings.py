0e8629b9794580b807bb9b29f4d32d2e71afa353
SofijnConsultingGroup <33628307+SofijnConsultingGroup@users.noreply.github.com>
2019-04-27 08:15:33 +0200

    ApptwoConfig --> AppTwoConfig

--- ProTwo/ProTwo/settings.py
+++ ProTwo/ProTwo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appTwo.apps.ApptwoConfig',
+    'appTwo.apps.AppTwoConfig',
 ]
 
 MIDDLEWARE = [
